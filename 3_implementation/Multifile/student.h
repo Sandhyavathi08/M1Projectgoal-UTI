b72d3842bfc054318a0bb8a0c018e87d94ba9511
Sandhyavathi08 <99087988+Sandhyavathi08@users.noreply.github.com>
2022-02-17 12:51:26 -0500

    Add files via upload

---
+++ 3_implementation/Multifile/student.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+struct student{
+    char rollno[15];
+    char name[20];
+    char address[20];
+    char parentsname[20];
+    int Class;
+    long unsigned int phone_no;
+};
+
+struct student stu;
---
+++ 3_implementation/Multifile/studentmain.c
@@ -0,0 +1,9 @@
+#include"student.h"
+int main(){
+    ClearConsoleToColors(17,15);
+    SetConsoleTitle("Student Record Management System");
+    window();
+   
+    main_window();
+    return 0;
+}
