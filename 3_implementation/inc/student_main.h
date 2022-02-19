43d39e36b374591d354aaedbf27bbfe1318c5f1c
Sandhyavathi08 <99087988+Sandhyavathi08@users.noreply.github.com>
2022-02-20 00:32:38 +0530

    Add files via upload

---
+++ 3_implementation/inc/student_main.h
@@ -0,0 +1,23 @@
+/**
+ * @file student_main.h
+ * @author sandhiya
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-20
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#ifndef __STUDENT_MAIN_H__
+#define __STUDENT_MAIN_H__
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+void show();
+void inserting();
+void searching();
+void Del();
+void Updation();
+#endif
