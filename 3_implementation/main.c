ba110797494f5f8f862856ff7f4431dda074747b
Sandhyavathi08 <99087988+Sandhyavathi08@users.noreply.github.com>
2022-02-20 00:25:54 +0530

    Add files via upload

---
+++ 3_implementation/main.c
@@ -0,0 +1,160 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+struct s
+{
+    int rolno;
+    char name[100];
+    char phone[100];
+    float percentage;
+    struct s *next;
+    
+}* R;
+void inserting(int rolno, char* name, char* phone, float percentage)
+{
+    
+    struct s * student = (struct s *) malloc(sizeof(struct s));
+    student->rolno= rolno;
+    strcpy(student->name, name);
+    strcpy(student->phone, phone);
+    student->percentage = percentage;
+    student->next = NULL;
+    
+    if(R==NULL){
+        R = student;
+    }
+    else{
+        student->next = R;
+        R = student;
+    }
+    
+}
+void searching(int rolno)
+{
+    struct s * t = R;
+    while(t!=NULL){
+        if(t->rolno==rolno){
+            printf("Rolno : %d\n", t->rolno);
+            printf("Name: %s\n", t->name);
+            printf("Phone: %s\n", t->phone);
+            printf("Percentage: %0.4f\n", t->percentage);
+            return;
+        }
+        t = t->next;
+    }
+    printf("Student with roll number %d is not found !!!\n", rolno);
+}
+void Updation(int rolno)
+{
+    
+    struct s * t = R;
+    while(t!=NULL){
+        
+        if(t->rolno==rolno){
+            printf("Record with roll number %d Found !!!\n", rolno);
+            printf("Enter new name: ");
+            scanf("%s", t->name);
+            printf("Enter new phone number: ");
+            scanf("%s", t->phone);
+            printf("Enter new percentage: ");
+            scanf("%f",&t->percentage);
+            printf("Updated\n");
+            return;
+        }
+        t = t->next;
+        
+    }
+    printf("Student with rolno %d is not found !!!\n", rolno);
+    
+}
+void Del(int rolno)
+{
+    struct s * t1 = R;
+    struct s * t2 = R; 
+    while(t1!=NULL){
+        
+        if(t1->rolno==rolno){
+            
+            printf("Record with roll number %d Found !!!\n", rolno);
+            
+            if(t1==t2){
+                R = R->next;
+                free(t1);
+            }
+            else{
+                t2->next = t1->next;
+                free(t1); 
+            }
+            
+            printf("Record Successfully Deleted !!!\n");
+            return;
+            
+        }
+        t2 = t1;
+        t1 = t1->next;
+        
+    }
+    printf("Student with roll number %d is not found !!!\n", rolno);
+    
+}
+void show()
+{
+    struct s* t =R;
+    while(t!=NULL){
+        
+        printf("Roll Number: %d\n", t->rolno);
+        printf("Name: %s\n", t->name);
+        printf("Phone: %s\n", t->phone);
+        printf("Percentage: %0.4f\n\n", t->percentage);
+        t = t->next;
+        
+    }
+}
+int main()
+{
+    R = NULL;
+    int c;
+    char name[100];
+    char phone[100];
+    int rolno;
+    float percent;
+    printf("1 insert student \n2 search for student details\n3  delete student details\n4  update student details\n5  display all student details");
+    do
+    {
+        printf("\nEnter Choice: ");
+        scanf("%d", &c);
+        switch (c)
+        {
+            case 1:
+                printf("Enter rolno: ");
+                scanf("%d", &rolno);
+                printf("Enter name: ");
+                scanf("%s", name);
+                printf("Enter phone no: ");
+                scanf("%s", phone);
+                printf("Enter percent: ");
+                scanf("%f", &percent);
+                inserting(rolno, name, phone, percent);
+                break;
+            case 2:
+                printf("Enter rolno ");
+                scanf("%d", &rolno);
+                searching(rolno);
+               break;
+            case 3:
+                printf("Enter rolno ");
+                scanf("%d", &rolno);
+                Del(rolno);
+                break;
+            case 4:
+                printf("Enter rolno  ");
+                scanf("%d", &rolno);
+                Updation(rolno);
+                break;
+            case 5:
+                show();
+                break;
+        }
+        
+    } while (c != 0);
+}
