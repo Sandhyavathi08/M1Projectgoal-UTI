d8eb10102df44b36abe4506648e88a830c54605d
Sandhyavathi08 <99087988+Sandhyavathi08@users.noreply.github.com>
2022-02-19 23:52:17 +0530

    Delete 3_implementation/Multifile directory

--- 3_implementation/Multifile/studentmain.c
+++
@@ -1,9 +0,0 @@
-#include"student.h"
-int main(){
-    ClearConsoleToColors(17,15);
-    SetConsoleTitle("Student Record Management System");
-    window();
-   
-    main_window();
-    return 0;
-}
